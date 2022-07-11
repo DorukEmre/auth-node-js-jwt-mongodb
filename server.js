4d2a4531a4d76d8196a442854690833f927bda0c
DorukEmre <doruk.emre@outlook.com>
2022-07-11 12:18:05 +0100

    Initialize Mongoose

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const db = require("./app/models");
+const dbConfig = require("./app/config/db.config");
+const Role = db.role;
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -16,6 +33,37 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'moderator' to roles collection");
+      });
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
